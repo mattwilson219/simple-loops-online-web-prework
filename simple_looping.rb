b967ec80f621c4b362c994428af07f2fe3e08c77
Matthew Wilson <wilsonmatt219@gmail.com>
2019-03-04 23:06:37 +0000

    Automatically backed up by Learn

--- simple_looping.rb
+++ simple_looping.rb
@@ -35,7 +35,7 @@ def while_iterator(number_of_times)
   phrase = "Welcome to Flatiron School's Web Development Course!"
   puts phrase
   counter += 1
-  
+end
 end
 
 def until_iterator(number_of_times)
